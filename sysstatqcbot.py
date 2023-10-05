785210333b0df8bd10bf71c2d09f625c90a66245
3225942953 <145473288+3225942953@users.noreply.github.com>
2023-10-05 11:43:45 +0800

    Update sysstatqcbot.py

--- sysstatqcbot.py
+++ sysstatqcbot.py
@@ -22,8 +22,11 @@ class SysStatQCBotPlugin:
             disk_info = psutil.disk_usage('/')
             cpu_ststs = psutil.cpu_stats()
             cpu_freq = psutil.cpu_freq()
+            system_name = platform.system()  # 获取系统名称（如：Linux、Windows、Darwin等）
+            system_version = platform.release()  # 获取系统版本号
             
             res = f"""【服务器当前状态】
+操作系统: {system_name} {system_version}
 进程内存占用: {core_mem:.2f}MB
 总内存: {sysmem_info.total / 1024 / 1024:.2f}MB
 已用内存: {sysmem_info.used / 1024 / 1024:.2f}MB
